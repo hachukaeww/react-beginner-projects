2717b5e232e09f28d1ef3d071fecbd3028ebbeb7
muhammad <ha4ukaev.m@gmail.com>
2022-08-27 15:57:33 +0300

    Counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import './index.scss';
 
 function App() {
+  const [count,setCount] = React.useState(0);
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={()=>setCount(count+1)} className="plus">Плюс +</button>
+        <button onClick={()=>setCount(count-1)} className="minus">- Минус</button>
       </div>
     </div>
   );
